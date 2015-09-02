18688413d0b27a48126473610377d8e4b6215c08
Andrew Lopez <andrewlopez88@gmail.com>
2015-09-02 10:46:31 -0700

    select via db ID#

---
+++ dev/singleItem.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+$searchString = $_POST['id'];
+require("mysql_connect.php");
+if (isset($_SESSION['user'])) {
+    $currentUser = $_SESSION['user'];
+    $query = "SELECT `title`, `details`, `created`, `updated`, `dueDate`, `status`, `priority` FROM `GroupTodo` WHERE `id` = $searchString";
+    $results = mysqli_query($conn, $query);
+    if (mysqli_num_rows($results) == 0) {
+        print("There are no to do list items yet, MAKE SOME!");
+    } else if (mysqli_num_rows($results) > 0) {
+        $list = mysqli_fetch_assoc($results);
+        $encoded_results = json_encode($list);
+        print_r($encoded_results);
+    }
+} else {
+    print("User not found, please log in");
+}
